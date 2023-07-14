80b281a40dc4ae0e176102beeb5a460208527d42
jeferson-viana <jefersonv28@gmail.com>
2023-07-14 20:58:49 -0300

    Requisito 7 concluído

---
+++ src/models/Category.js
@@ -0,0 +1,22 @@
+module.exports = (sequelize, DataTypes) => {
+  const Category = sequelize.define(
+    'Category',
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+    },
+    {
+      tableName: 'categories',
+      underscored: true,
+      timestamps: false,
+    },
+  );
+  return Category;
+};
